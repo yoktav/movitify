afd4046b0ca85482eb5ed5fb4a297c91334c7813
yoktav <iletisimyilmazoktav@gmail.com>
2021-01-08 02:11:27 +0300

    Create movie-database api

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~/plugins/services/api/movie-database.js',
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
