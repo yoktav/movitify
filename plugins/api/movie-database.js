efc317b6e4eb6ce2f777d8dee736d441fb22f268
yoktav <iletisimyilmazoktav@gmail.com>
2021-02-05 16:09:02 +0300

    Seperate search functions

--- plugins/api/movie-database.js
+++ plugins/api/movie-database.js
@@ -5,35 +5,34 @@ export default (content, inject) => {
   content.$api = api;
 };
 
-const BASE_URL = process.env.NUXT_ENV_DOMAIN;
-const SEARCH_BY_QUERY_STATE = 'searchByQuery';
-const SEARCH_BY_ID_STATE = 'searchById';
+const BASE_URL = process.env.NUXT_ENV_PRODUCT_DOMAIN;
 
 const END_POINTS = {
   searchByQuery: `${BASE_URL}/api/q`,
   searchById: `${BASE_URL}/api/id`,
 };
 
-async function searchMovie(method, query) {
-  let url = null;
-
-  if (method === SEARCH_BY_QUERY_STATE) {
-    url = `${END_POINTS.searchByQuery}/${query}`;
-  } else if (method === SEARCH_BY_ID_STATE) {
-    url = `${END_POINTS.searchById}/${query}`;
-  } else {
-    return 'Error: Check your method!';
-  }
+async function searchById(searchQuery) {
+  const url = `${END_POINTS.searchById}/${searchQuery}`;
 
   const movies = await fetchData(url);
   return movies.data;
 }
 
+async function searchByQuery(searchQuery) {
+  const url = `${END_POINTS.searchByQuery}/${searchQuery}`;
+
+  const movies = await fetchData(url);
+  // console.log(movies);
+
+  return movies.data;
+}
+
 const api = {
   searchByQuery(query) {
-    return searchMovie(SEARCH_BY_QUERY_STATE, `${query}`);
+    return searchByQuery(`${query}`);
   },
   searchById(query) {
-    return searchMovie(SEARCH_BY_ID_STATE, `${query}`);
+    return searchById(`${query}`);
   },
 };
--- services/api/movie-database.js
+++ services/api/movie-database.js
@@ -9,28 +9,30 @@ const redis = require('redis');
 const client = redis.createClient(REDIS_PORT);
 const app = express();
 
-const BASE_URL = process.env.NUXT_ENV_OMDB_BASE_URL;
-const API_KEY = process.env.NUXT_ENV_OMDB_API_KEY;
-const SEARCH_BY_QUERY_STATE = 'searchByQuery';
-const SEARCH_BY_ID_STATE = 'searchById';
+const BASE_URL = process.env.NUXT_ENV_MOVIE_DB_BASE_URL;
+const API_KEY = process.env.NUXT_ENV_MOVIE_DB_API_KEY;
 
 const END_POINTS = {
   searchByQuery: `${BASE_URL}/?apikey=${API_KEY}&s`,
   searchById: `${BASE_URL}/?apikey=${API_KEY}&i`,
 };
 
-async function searchMovie(method, searchQuery) {
-  let url = null;
-
-  if (method === SEARCH_BY_QUERY_STATE) {
-    url = `${END_POINTS.searchByQuery}=${searchQuery}`;
-  } else if (method === SEARCH_BY_ID_STATE) {
-    url = `${END_POINTS.searchById}=${searchQuery}`;
-  } else {
-    return 'Error: Check your method!';
-  }
-
+async function searchById(searchQuery) {
+  const url = `${END_POINTS.searchById}=${searchQuery}`;
   const movies = await fetchData(url);
+
+  const data = JSON.stringify(movies.data);
+
+  // Set data to Redis
+  client.setex(searchQuery, ONE_HOUR, data);
+
+  return data;
+}
+
+async function searchByQuery(searchQuery) {
+  const url = `${END_POINTS.searchByQuery}=${searchQuery}`;
+  const movies = await fetchData(url);
+
   const data = JSON.stringify(movies.data);
 
   // Set data to Redis
@@ -55,7 +57,7 @@ function cache(request, response, next) {
 }
 
 app.get('/q/:slug', cache, function (request, response) {
-  searchMovie(SEARCH_BY_QUERY_STATE, request.params.slug)
+  searchByQuery(request.params.slug)
     .then(request => {
       response.end(request);
     })
@@ -65,7 +67,7 @@ app.get('/q/:slug', cache, function (request, response) {
 });
 
 app.get('/id/:slug', cache, function (request, response) {
-  searchMovie(SEARCH_BY_ID_STATE, request.params.slug)
+  searchById(request.params.slug)
     .then(request => {
       response.end(request);
     })
