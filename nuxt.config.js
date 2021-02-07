7af7fb6529b3a2249992595adfd1341ac0b22685
yoktav <iletisimyilmazoktav@gmail.com>
2021-02-07 17:23:18 +0300

    Configure html lang attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
     title: 'movitify',
     meta: [
       { charset: 'utf-8' },
