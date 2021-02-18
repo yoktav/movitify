9578beb19ab4383192a29b574791d95422af298d
yoktav <iletisimyilmazoktav@gmail.com>
2021-02-19 01:35:00 +0300

    Register all components for jest

---
+++ components/jest.components.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+import Header from './Header/Header.vue';
+import Icon from './Icon/Icon.vue';
+import MovieCard from './MovieCard/MovieCard.vue';
+import Poster from './Poster/Poster.vue';
+import Search from './Search/Search.vue';
+
+Vue.component('Header', Header);
+Vue.component('Icon', Icon);
+Vue.component('MovieCard', MovieCard);
+Vue.component('Poster', Poster);
+Vue.component('Search', Search);
