a42d349cfbe58c320e4c5b0819d3a84772469eb6
yoktav <iletisimyilmazoktav@gmail.com>
2021-03-07 22:24:25 +0300

    Add describe functions multiple tests in poster unit test

--- components/Poster/Poster.spec.js
+++ components/Poster/Poster.spec.js
@@ -1,31 +1,39 @@
-import { mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Poster from './Poster.vue';
 
 describe('Poster.vue', () => {
-  const modifierClasses = ['u-color-red', 'u-display-flex', 'u-align-items-center'];
-  modifierClasses.forEach(modifierClass => {
-    it(`is modifierClass property can have '${modifierClass}'`, () => {
-      const wrapper = mount(Poster, {
-        propsData: {
-          modifierClass: modifierClass,
-        },
+  const localVue = createLocalVue();
+
+  describe('Check modifier_class working properly', () => {
+    const modifierClasses = ['u-color-red', 'u-display-flex', 'u-align-items-center'];
+    modifierClasses.forEach(modifierClass => {
+      it(`Can modifierClass property have '${modifierClass}'`, () => {
+        const wrapper = mount(Poster, {
+          localVue,
+          propsData: {
+            modifierClass: modifierClass,
+          },
+        });
+        expect(wrapper.classes()).toContain(modifierClass);
       });
-      expect(wrapper.classes()).toContain(modifierClass);
     });
   });
 
-  const imageAlts = ['Movie Poster', 'Series Poster', 'Actor Poster'];
-  imageAlts.forEach(imageAlt => {
-    it(`is imageAltc can have '${imageAlt}'`, () => {
-      const wrapper = mount(Poster, {
-        propsData: {
-          posterSrc: '',
-          posterAlt: imageAlt,
-        },
+  describe('Check imageAlt working properly', () => {
+    const imageAlts = ['Movie Poster', 'Series Poster', 'Actor Poster'];
+    imageAlts.forEach(imageAlt => {
+      it(`is imageAltc can have '${imageAlt}'`, () => {
+        const wrapper = mount(Poster, {
+          localVue,
+          propsData: {
+            posterSrc: '',
+            posterAlt: imageAlt,
+          },
+        });
+        const img = wrapper.find('img');
+        const altAttribute = img.attributes('alt');
+        expect(altAttribute).toContain(imageAlt);
       });
-      const img = wrapper.find('img');
-      const altAttribute = img.attributes('alt');
-      expect(altAttribute).toContain(imageAlt);
     });
   });
 });
