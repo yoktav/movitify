ae4fdc421fd3bca2179b8312d6a42a9812cbddd9
yoktav <iletisimyilmazoktav@gmail.com>
2021-03-13 12:26:01 +0300

    Add NuxtLink to jest components

--- components/jest.components.js
+++ components/jest.components.js
@@ -1,3 +1,7 @@
+//
+// Imports
+//
+
 import Vue from 'vue';
 
 import Header from './Header/Header.vue';
@@ -6,10 +10,28 @@ import MovieCard from './MovieCard/MovieCard.vue';
 import Poster from './Poster/Poster.vue';
 import Search from './Search/Search.vue';
 
+//
+// Components
+//
+
 Vue.component('Header', Header);
 Vue.component('Icon', Icon);
 Vue.component('MovieCard', MovieCard);
 Vue.component('Poster', Poster);
 Vue.component('Search', Search);
+Vue.component('NuxtLink', {
+  props: {
+    to: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+  template: '<a href="#"><slot>NuxtLink</slot></a>',
+});
+
+//
+// Directives
+//
 
 Vue.directive('lazy-load', {});
