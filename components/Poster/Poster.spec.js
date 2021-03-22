d9bda84c5ca516953e65c563124b35b8a28c61a5
yoktav <iletisimyilmazoktav@gmail.com>
2021-03-22 10:35:40 +0300

    Add empty spaces to Poster component unit test

--- components/Poster/Poster.spec.js
+++ components/Poster/Poster.spec.js
@@ -1,11 +1,12 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import Poster from './Poster.vue';
 
-describe('Poster.vue', () => {
+describe('Component Poster.vue', () => {
   const localVue = createLocalVue();
 
   describe('Check modifier_class working properly', () => {
     const modifierClasses = ['u-color-red', 'u-display-flex', 'u-align-items-center'];
+
     modifierClasses.forEach(modifierClass => {
       it(`Can modifierClass property have '${modifierClass}'`, () => {
         const wrapper = mount(Poster, {
@@ -14,6 +15,7 @@ describe('Poster.vue', () => {
             modifierClass: modifierClass,
           },
         });
+
         expect(wrapper.classes()).toContain(modifierClass);
       });
     });
@@ -21,6 +23,7 @@ describe('Poster.vue', () => {
 
   describe('Check imageAlt working properly', () => {
     const imageAlts = ['Movie Poster', 'Series Poster', 'Actor Poster'];
+
     imageAlts.forEach(imageAlt => {
       it(`is imageAltc can have '${imageAlt}'`, () => {
         const wrapper = mount(Poster, {
@@ -30,8 +33,10 @@ describe('Poster.vue', () => {
             posterAlt: imageAlt,
           },
         });
+
         const img = wrapper.find('img');
         const altAttribute = img.attributes('alt');
+
         expect(altAttribute).toContain(imageAlt);
       });
     });
