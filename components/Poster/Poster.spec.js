bc27ac4882d18cc8e0196d24ec81e4c6472812e5
yoktav <iletisimyilmazoktav@gmail.com>
2021-03-22 12:37:46 +0300

    Add missing tests to poster component

--- components/Poster/Poster.spec.js
+++ components/Poster/Poster.spec.js
@@ -4,7 +4,7 @@ import Poster from './Poster.vue';
 describe('Component Poster.vue', () => {
   const localVue = createLocalVue();
 
-  describe('Check modifier_class working properly', () => {
+  describe('Check modifierClass working properly', () => {
     const modifierClasses = ['u-color-red', 'u-display-flex', 'u-align-items-center'];
 
     modifierClasses.forEach(modifierClass => {
@@ -21,11 +21,11 @@ describe('Component Poster.vue', () => {
     });
   });
 
-  describe('Check imageAlt working properly', () => {
+  describe('Check posterAlt working properly', () => {
     const imageAlts = ['Movie Poster', 'Series Poster', 'Actor Poster'];
 
     imageAlts.forEach(imageAlt => {
-      it(`is imageAltc can have '${imageAlt}'`, () => {
+      it(`is posterAlt can have '${imageAlt}'`, () => {
         const wrapper = mount(Poster, {
           localVue,
           propsData: {
@@ -38,6 +38,34 @@ describe('Component Poster.vue', () => {
         const altAttribute = img.attributes('alt');
 
         expect(altAttribute).toContain(imageAlt);
+      });
+    });
+  });
+
+  describe('Check posterSrc working properly', () => {
+    const posterSrcSets = ['/dummpy.png', '/dummpy.svg', '/dummpy.mp4'];
+
+    posterSrcSets.forEach(posterSrc => {
+      it(`is posterSrc can have '${posterSrc}'`, () => {
+        const wrapper = mount(Poster, {
+          localVue,
+          propsData: {
+            posterSrc,
+            posterAlt: '',
+          },
+        });
+
+        const img = wrapper.find('img');
+        const srcAttribute = img.attributes('data-src');
+
+        // Can check this due to nuxt-lazy-load triggers only when element is visible /// to user
+        //
+        // const imageErrorFallback = jest.spyOn(wrapper.vm, 'imageErrorFallback');
+        // if (posterSrc.includes('mp4')) {
+        //   expect(imageErrorFallback).toHaveBeenCalled();
+        // }
+
+        expect(srcAttribute).toContain(posterSrc);
       });
     });
   });
